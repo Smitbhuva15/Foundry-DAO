69aa9d505a9c4bd61f004a0664aaab57e5fa8e32
Smitbhuva15 <itssmit123@gmail.com>
2025-06-13 14:47:31 +0000

    Tests added

--- test/GovernorTest.t.sol
+++ test/GovernorTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
-import {Test} from 'forge-std/Test.sol';
+import {Test,console} from 'forge-std/Test.sol';
 import {Box} from '../src/Box.sol';
 import {GovToken} from '../src/GovToken.sol';
 import {MyGovernor} from '../src/MyGovernor.sol';
@@ -54,4 +54,49 @@ contract GovernorTest is Test{
         box.store(1);
     }
 
+
+        function testGovernanceUpdatesBox() public {
+        uint256 valueToStore = 777;
+        string memory description = "Store 1 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature("store(uint256)", valueToStore);
+        addressesToCall.push(address(box));
+        values.push(0);
+        functionCalls.push(encodedFunctionCall);
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(addressesToCall, values, functionCalls, description);
+
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+      
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+
+        // 2. Vote
+        string memory reason = "I like a do da cha cha";
+        // 0 = Against, 1 = For, 2 = Abstain for this example
+
+        uint8 voteWay = 1;
+        vm.prank(VOTER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+
+        // 3. Queue
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(addressesToCall, values, functionCalls, descriptionHash);
+        vm.roll(block.number + MIN_DELAY + 1);
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+
+        // 4. Execute
+        governor.execute(addressesToCall, values, functionCalls, descriptionHash);
+
+        assert(box.retrieve() == valueToStore);
+    }
 }
+
+
